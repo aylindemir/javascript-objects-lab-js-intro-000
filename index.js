bbdc2a76564bad30c1efc43684ec68f4ad447a26
Aylin Demir <aylin.de@outlook.com>
2019-12-26 20:26:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ function updateObjectWithKeyAndValue(object, key, value){
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return Object.assign(object,{[key]:value})
 }
+
+function deleteFromObjectByKey(object, key){
+  var targetobject = object;
+  // delete targetobject[key]
+  return delete targetobject[key]
+}
